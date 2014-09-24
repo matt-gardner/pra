e2ef94dbe87a343e3d1c697c2b2b208f25f6f5b1
Matt Gardner <mg1@cs.cmu.edu>
2014-09-24 09:50:27 -0700

    Rewrote PairComparator to have better typing.

--- src/main/java/edu/cmu/ml/rtw/util/PairComparator.java
+++ src/main/java/edu/cmu/ml/rtw/util/PairComparator.java
@@ -1,75 +1,137 @@
-/**
- *
- */
 package edu.cmu.ml.rtw.util;
 
 import java.util.Comparator;
 
-/**
- * @author acarlson, mwgreave
- *
- * This class provides a comparator that can be used to sort a collection of
- * Pair objects by either the left or right elements in the Pairs.
- *
- * The types used in instantiating your PairComparator must match the types in
- * the Pairs in the collection. Furthermore, both types must implement the
- * Comparable interface.
- *
- * Example usage:
- *
- * List<Pair<NGram, Double> > myList = ...;
- *
- * To sort by the right elements:
- *
- * Comparator<Pair<NGram, Double>> right_comparator = new PairComparator<NGram,
- * Double>(PairComparator.Side.RIGHT);
- *
- * Collections.sort(myList, right_comparator);
- *
- * To sort by the left elements:
- *
- * Comparator<Pair<NGram, Double>> left_comparator = new PairComparator<NGram,
- * Double>(PairComparator.Side.LEFT);
- *
- * Collections.sort(myList, left_comparator);
- *
- * (mwgreave)
- * To sort in descending order by the left or right value
- * use NEGLEFT and NEGRIGHT, respectively.
- *
- * e.g.
- *
- * Comparator<Pair<NGram, Double>> right_comparator = new PairComparator<NGram,
- * Double>(PairComparator.Side.NEGRIGHT);
- *   will compare on the right element and allow one to sort a list of Pairs
- *   with this comparator into descending order.
- */
-
-public class PairComparator<A extends Comparable<? super A>, B extends Comparable<? super B>>
-    implements Comparator<Pair<A, B>> {
-
-  public enum Side {
-    LEFT,     // order by LEFT item; for ascending order
-    RIGHT,    // order by RIGHT item; for ascending order
-
-    NEGLEFT,  // order by LEFT item; for descending order
-    NEGRIGHT  // order by RIGHT item; for descending order
-  }
-  // Determines *how* the comparator compares two items.
-  Side comparisonSide;
-
-  public PairComparator(Side c) {
-    comparisonSide = c;
+public class PairComparator {
+  public static <L, R extends Comparable<? super R>> Comparator<Pair<L, R>> negativeRight() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        return -pair1.getRight().compareTo(pair2.getRight());
+      }
+    };
   }
 
-  public int compare(Pair<A, B> p1, Pair<A, B> p2) {
-    if (comparisonSide == Side.LEFT)
-      return p1.getLeft().compareTo(p2.getLeft());
-    else if(comparisonSide == Side.RIGHT)
-      return p1.getRight().compareTo(p2.getRight());
-    else if(comparisonSide == Side.NEGLEFT)
-      return -(p1.getLeft().compareTo(p2.getLeft()));
-    else // NEGRIGHT
-      return -(p1.getRight().compareTo(p2.getRight()));
+  public static <L, R extends Comparable<? super R>> Comparator<Pair<L, R>> right() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        return pair1.getRight().compareTo(pair2.getRight());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R> Comparator<Pair<L, R>> negativeLeft() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        return -pair1.getLeft().compareTo(pair2.getLeft());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R> Comparator<Pair<L, R>> left() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        return pair1.getLeft().compareTo(pair2.getLeft());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> rightThenLeft() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int rightComparison = pair1.getRight().compareTo(pair2.getRight());
+        if (rightComparison != 0) return rightComparison;
+        return pair1.getLeft().compareTo(pair2.getLeft());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> rightThenNegativeLeft() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int rightComparison = pair1.getRight().compareTo(pair2.getRight());
+        if (rightComparison != 0) return rightComparison;
+        return -pair1.getLeft().compareTo(pair2.getLeft());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> negativeRightThenLeft() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int rightComparison = -pair1.getRight().compareTo(pair2.getRight());
+        if (rightComparison != 0) return rightComparison;
+        return pair1.getLeft().compareTo(pair2.getLeft());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> negativeRightThenNegativeLeft() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int rightComparison = -pair1.getRight().compareTo(pair2.getRight());
+        if (rightComparison != 0) return rightComparison;
+        return -pair1.getLeft().compareTo(pair2.getLeft());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> leftThenRight() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int leftComparison = pair1.getLeft().compareTo(pair2.getLeft());
+        if (leftComparison != 0) return leftComparison;
+        return pair1.getRight().compareTo(pair2.getRight());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> leftThenNegativeRight() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int leftComparison = pair1.getLeft().compareTo(pair2.getLeft());
+        if (leftComparison != 0) return leftComparison;
+        return -pair1.getRight().compareTo(pair2.getRight());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> negativeLeftThenRight() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int leftComparison = -pair1.getLeft().compareTo(pair2.getLeft());
+        if (leftComparison != 0) return leftComparison;
+        return pair1.getRight().compareTo(pair2.getRight());
+      }
+    };
+  }
+
+  public static <L extends Comparable<? super L>, R extends Comparable<? super R>>
+      Comparator<Pair<L, R>> negativeLeftThenNegativeRight() {
+    return new Comparator<Pair<L, R>>() {
+      @Override
+      public int compare(Pair<L, R> pair1, Pair<L, R> pair2) {
+        int leftComparison = -pair1.getLeft().compareTo(pair2.getLeft());
+        if (leftComparison != 0) return leftComparison;
+        return -pair1.getRight().compareTo(pair2.getRight());
+      }
+    };
   }
 }
