9652f2c3f38bdda5ef14638f5b3a7fe29cbabc96
Matt Gardner <mg1@cs.cmu.edu>
2014-11-11 11:29:41 -0500

    Fixed some tests, added a new one

--- src/main/java/edu/cmu/ml/rtw/pra/experiments/DatasetFactory.java
+++ src/main/java/edu/cmu/ml/rtw/pra/experiments/DatasetFactory.java
@@ -1,13 +1,14 @@
 package edu.cmu.ml.rtw.pra.experiments;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 import edu.cmu.ml.rtw.users.matt.util.Dictionary;
 
 public class DatasetFactory {
   public Dataset fromFile(String path, Dictionary dict) throws IOException {
-    return Dataset.readFromFile(path, dict);
+    return fromReader(new BufferedReader(new FileReader(path)), dict);
   }
 
   public Dataset fromReader(BufferedReader reader, Dictionary dict) throws IOException {
