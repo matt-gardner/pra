daad33696ede58d24b33ee5df22cdec8cb1ae06a
Matt Gardner <mg1@cs.cmu.edu>
2015-01-29 00:11:16 -0500

    Need to mkdir before writing a temporary file

--- src/main/scala/edu/cmu/ml/rtw/pra/graphs/GraphDensifier.scala
+++ src/main/scala/edu/cmu/ml/rtw/pra/graphs/GraphDensifier.scala
@@ -28,6 +28,7 @@ class GraphDensifier(
     val dense_edge_vectors = edge_vectors.par.map(x => (x._1, similarity_matrix * x._2))
     val tmp_matrix_file = matrix_out_dir + "tmp_graph.tsv"
     println(s"Writing edges temporarily to $tmp_matrix_file")
+    file_util.mkdirs(matrix_out_dir)
     val writer = file_util.getFileWriter(tmp_matrix_file)
     dense_edge_vectors.seq.map(x => {
       var offset = 0
