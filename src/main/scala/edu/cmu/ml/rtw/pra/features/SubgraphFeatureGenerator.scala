e42eba45baebb98cdaac2cc4d122540c24685706
Matt Gardner <mg1@cs.cmu.edu>
2015-12-14 12:45:02 -0500

    Made SubgraphFeatureGenerator much more memory efficient

--- src/main/scala/edu/cmu/ml/rtw/pra/features/SubgraphFeatureGenerator.scala
+++ src/main/scala/edu/cmu/ml/rtw/pra/features/SubgraphFeatureGenerator.scala
@@ -46,9 +46,9 @@ class SubgraphFeatureGenerator(
   override def createTestMatrix(data: Dataset): FeatureMatrix = createMatrixFromData(data)
 
   def createMatrixFromData(data: Dataset) = {
-    val subgraphs = getLocalSubgraphs(data)
-    Outputter.outputAtLevel(s"Done getting subgraphs; extracting features", logLevel)
-    extractFeatures(subgraphs)
+    Outputter.outputAtLevel(s"Creating feature matrix from ${data.instances.size} instances", logLevel)
+    val rows = data.instances.par.map(constructMatrixRow).flatten.seq
+    new FeatureMatrix(rows.asJava)
   }
 
   override def getFeatureNames(): Array[String] = {
