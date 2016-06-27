21ca5f25283914c7675f7dfcfbd53ac0be293e6d
Matt Gardner <mattg@allenai.org>
2016-06-27 21:00:20 +0000

    Added a max instances parameter to SplitCreator
    I also fixed some formatting issues.

--- src/main/scala/edu/cmu/ml/rtw/pra/data/SplitCreator.scala
+++ src/main/scala/edu/cmu/ml/rtw/pra/data/SplitCreator.scala
@@ -17,6 +17,8 @@ import org.json4s.native.JsonMethods.{pretty,render}
 
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.util.Random
+
 object SplitCreator {
   def create(params: JValue, praBase: String, fileUtil: FileUtil): SplitCreator = {
     val splitTypes = Seq("fixed split from relation metadata", "add negatives to split")
@@ -36,6 +38,8 @@ abstract class SplitCreator(
 ) extends Step(Some(params), fileUtil) with LazyLogging {
   implicit val formats = DefaultFormats
   override val name = "Split Creator"
+  val baseParamKeys = Seq("name", "type", "relation metadata", "graph", "negative instances",
+    "generate negatives for", "max instances")
 
   val baseSplitDir = praBase + "splits/"
   val splitName = (params \ "name").extract[String]
@@ -60,28 +64,29 @@ abstract class SplitCreator(
     }
   }
   val negativeExampleSelector = createNegativeExampleSelector(params \ "negative instances")
+  val maxInstances = JsonHelper.extractAsOption[Int](params, "max instances")
+  val random = new Random
 
   def _runStep() {
-    fileUtil.mkdirOrDie(splitDir)
     createSplit()
   }
   def createSplit()
 
   def addNegativeExamples(
       data: Dataset[NodePairInstance],
-      other_positive_instances: Seq[NodePairInstance],
+      otherPositiveInstances: Seq[NodePairInstance],
       relation: String,
       domains: Map[String, String],
       ranges: Map[String, String],
-      node_dict: Dictionary): Dataset[NodePairInstance] = {
+      nodeDict: Dictionary): Dataset[NodePairInstance] = {
     val domain = if (domains == null) null else domains(relation)
     val allowedSources = if (domain == null) None else {
       val fixed = domain.replace("/", "_")
-      val domain_file = s"${relationMetadata}category_instances/${fixed}"
-      if (fileUtil.fileExists(domain_file)) {
+      val domainFile = s"${relationMetadata}category_instances/${fixed}"
+      if (fileUtil.fileExists(domainFile)) {
         // The true in this line says that we should drop nodes from the set if they don't show up
         // in the dictionary.
-        Some(fileUtil.readIntegerSetFromFile(domain_file, Some(node_dict), true))
+        Some(fileUtil.readIntegerSetFromFile(domainFile, Some(nodeDict), true))
       } else {
         logger.warn(s"Didn't find a category file for type $domain (domain of relation $relation)")
         None
@@ -90,17 +95,17 @@ abstract class SplitCreator(
     val range = if (ranges == null) null else ranges(relation)
     val allowedTargets = if (range == null) None else {
       val fixed = range.replace("/", "_")
-      val range_file = s"${relationMetadata}category_instances/${fixed}"
-      if (fileUtil.fileExists(range_file)) {
+      val rangeFile = s"${relationMetadata}category_instances/${fixed}"
+      if (fileUtil.fileExists(rangeFile)) {
         // The true in this line says that we should drop nodes from the set if they don't show up
         // in the dictionary.
-        Some(fileUtil.readIntegerSetFromFile(range_file, Some(node_dict), true))
+        Some(fileUtil.readIntegerSetFromFile(rangeFile, Some(nodeDict), true))
       } else {
         logger.warn(s"Didn't find a category file for type $range (range of relation $relation)")
         None
       }
     }
-    negativeExampleSelector.selectNegativeExamples(data, other_positive_instances, allowedSources, allowedTargets)
+    negativeExampleSelector.selectNegativeExamples(data, otherPositiveInstances, allowedSources, allowedTargets)
   }
 
   def createNegativeExampleSelector(params: JValue): PprNegativeExampleSelector = {
@@ -120,8 +125,7 @@ class SplitCreatorFromMetadata(
   fileUtil: FileUtil
 ) extends SplitCreator(params, praBase, fileUtil) {
 
-  val paramKeys = Seq("name", "type", "relation metadata", "graph", "negative instances",
-    "generate negatives for", "relations", "percent training")
+  val paramKeys = baseParamKeys ++ Seq("generate negatives for", "relations", "percent training")
   JsonHelper.ensureNoExtras(params, name, paramKeys)
 
   override val inputs: Set[(String, Option[Step])] = Set(relationMetadataInput) ++ graphInput.toSet
@@ -132,39 +136,44 @@ class SplitCreatorFromMetadata(
     val relations = (params \ "relations").extract[List[String]]
     if (relations.size == 0) throw new IllegalStateException("You forgot to specify which "
       + "relations to run!")
-    fileUtil.touchFile(inProgressFile)
-    val params_out = fileUtil.getFileWriter(paramFile)
-    params_out.write(pretty(render(params)))
-    params_out.close
 
     val graph = new GraphOnDisk(graphDir, fileUtil)
 
-    val range_file = s"${relationMetadata}ranges.tsv"
-    val ranges = if (fileUtil.fileExists(range_file)) fileUtil.readMapFromTsvFile(range_file) else null
-    val domain_file = s"${relationMetadata}domains.tsv"
-    val domains = if (fileUtil.fileExists(domain_file)) fileUtil.readMapFromTsvFile(domain_file) else null
+    val rangeFile = s"${relationMetadata}ranges.tsv"
+    val ranges = if (fileUtil.fileExists(rangeFile)) fileUtil.readMapFromTsvFile(rangeFile) else null
+    val domainFile = s"${relationMetadata}domains.tsv"
+    val domains = if (fileUtil.fileExists(domainFile)) fileUtil.readMapFromTsvFile(domainFile) else null
 
-    val relations_to_run = fileUtil.getFileWriter(s"${splitDir}relations_to_run.tsv")
+    val relationsToRun = fileUtil.getFileWriter(s"${splitDir}relations_to_run.tsv")
     for (relation <- relations) {
-      relations_to_run.write(s"${relation}\n")
+      relationsToRun.write(s"${relation}\n")
     }
-    relations_to_run.close()
+    relationsToRun.close()
 
     for (relation <- relations) {
       val fixed = relation.replace("/", "_")
-      val relation_file = s"${relationMetadata}relations/${fixed}"
-      val all_instances = DatasetReader.readNodePairFile(relation_file, Some(graph), fileUtil)
+      val relationFile = s"${relationMetadata}relations/${fixed}"
+      val allInstances = DatasetReader.readNodePairFile(relationFile, Some(graph), fileUtil)
       val data = if (negativeExampleSelector == null) {
-        all_instances
+        allInstances
       } else {
-        addNegativeExamples(all_instances, Seq(), relation, domains.toMap, ranges.toMap, graph.nodeDict)
+        addNegativeExamples(allInstances, Seq(), relation, domains.toMap, ranges.toMap, graph.nodeDict)
       }
       logger.info("Splitting data")
       val (training, testing) = data.splitData(percentTraining)
-      val rel_dir = s"${splitDir}${fixed}/"
-      fileUtil.mkdirs(rel_dir)
-      fileUtil.writeLinesToFile(s"${rel_dir}training.tsv", training.instancesToStrings)
-      fileUtil.writeLinesToFile(s"${rel_dir}testing.tsv", testing.instancesToStrings)
+      val relDir = s"${splitDir}${fixed}/"
+      fileUtil.mkdirs(relDir)
+
+      val keptTrainingLines = maxInstances match {
+        case None => training.instancesToStrings
+        case Some(max) => random.shuffle(training.instancesToStrings).take(max)
+      }
+      val keptTestingLines = maxInstances match {
+        case None => testing.instancesToStrings
+        case Some(max) => random.shuffle(testing.instancesToStrings).take(max)
+      }
+      fileUtil.writeLinesToFile(s"${relDir}training.tsv", keptTrainingLines)
+      fileUtil.writeLinesToFile(s"${relDir}testing.tsv", keptTestingLines)
     }
     fileUtil.deleteFile(inProgressFile)
   }
@@ -176,80 +185,82 @@ class SplitCreatorFromExistingSplit(
   fileUtil: FileUtil
 ) extends SplitCreator(params, praBase, fileUtil) {
 
-  val paramKeys = Seq("name", "type", "relation metadata", "graph", "negative instances",
-    "generate negatives for", "from split")
+  val paramKeys = baseParamKeys ++ Seq("from split")
   JsonHelper.ensureNoExtras(params, name, paramKeys)
 
   val splitInput = Split.getStepInput(params \ "from split", praBase, fileUtil)
+  val fromSplitDir = baseSplitDir + (params \ "from split").extract[String] + "/"
+  val fromSplitInput = (fromSplitDir, None)
   override val inputs: Set[(String, Option[Step])] =
-    Set(relationMetadataInput, splitInput) ++ graphInput.toSet
+    Set(relationMetadataInput, splitInput, fromSplitInput) ++ graphInput.toSet
 
   override def createSplit() {
     logger.info(s"Creating split at $splitDir")
 
     val graph = new GraphOnDisk(graphDir, fileUtil)
 
-    val range_file = s"${relationMetadata}ranges.tsv"
-    val ranges = if (fileUtil.fileExists(range_file)) fileUtil.readMapFromTsvFile(range_file) else null
-    val domain_file = s"${relationMetadata}domains.tsv"
-    val domains = if (fileUtil.fileExists(domain_file)) fileUtil.readMapFromTsvFile(domain_file) else null
+    val rangeFile = s"${relationMetadata}ranges.tsv"
+    val ranges = if (fileUtil.fileExists(rangeFile)) fileUtil.readMapFromTsvFile(rangeFile) else null
+    val domainFile = s"${relationMetadata}domains.tsv"
+    val domains = if (fileUtil.fileExists(domainFile)) fileUtil.readMapFromTsvFile(domainFile) else null
 
-    val fromSplitDir = baseSplitDir + (params \ "from split").extract[String] + "/"
     val relations = fileUtil.readLinesFromFile(fromSplitDir + "relations_to_run.tsv")
-    val relations_to_run = fileUtil.getFileWriter(s"${splitDir}relations_to_run.tsv")
+    val relationsToRun = fileUtil.getFileWriter(s"${splitDir}relations_to_run.tsv")
     for (relation <- relations) {
-      relations_to_run.write(s"${relation}\n")
+      relationsToRun.write(s"${relation}\n")
     }
-    relations_to_run.close()
+    relationsToRun.close()
 
     for (relation <- relations) {
       val fixed = relation.replace("/", "_")
-      val rel_dir = s"${splitDir}${fixed}/"
-      fileUtil.mkdirs(rel_dir)
+      val relDir = s"${splitDir}${fixed}/"
+      fileUtil.mkdirs(relDir)
 
-      val training_file = s"${fromSplitDir}${fixed}/training.tsv"
-      val training_data = if (fileUtil.fileExists(training_file)) {
-        val training_instances = DatasetReader.readNodePairFile(training_file, Some(graph), fileUtil)
-        val filtered_training_instances = training_instances.instances
+      val trainingFile = s"${fromSplitDir}${fixed}/training.tsv"
+      val trainingData = if (fileUtil.fileExists(trainingFile)) {
+        val trainingInstances = DatasetReader.readNodePairFile(trainingFile, Some(graph), fileUtil)
+        val filteredTrainingInstances = trainingInstances.instances
           .filterNot(i => i.source == -1 || i.target == -1)
-        new Dataset[NodePairInstance](filtered_training_instances)
+        new Dataset[NodePairInstance](filteredTrainingInstances)
       } else {
         new Dataset[NodePairInstance](Seq())
       }
 
-      if (fileUtil.fileExists(training_file)) {
-        val negative_training_instances = if (generateNegativesFor.contains("training")) {
-          addNegativeExamples(training_data, Seq(), relation, domains.toMap, ranges.toMap, graph.nodeDict)
+      if (fileUtil.fileExists(trainingFile)) {
+        val negativeTrainingInstances = if (generateNegativesFor.contains("training")) {
+          addNegativeExamples(trainingData, Seq(), relation, domains.toMap, ranges.toMap, graph.nodeDict)
         } else {
           new Dataset[NodePairInstance](Seq())
         }
-        val new_training_file = s"${rel_dir}training.tsv"
-        fileUtil.copy(training_file, new_training_file)
-        fileUtil.writeLinesToFile(
-          new_training_file,
-          negative_training_instances.instancesToStrings,
-          append=true
-        )
+        val newTrainingFile = s"${relDir}training.tsv"
+        val allLines = fileUtil.readLinesFromFile(trainingFile) ++ negativeTrainingInstances.instancesToStrings
+        val keptLines = maxInstances match {
+          case None => allLines
+          case Some(max) => random.shuffle(allLines).take(max)
+        }
+        logger.info(s"maxInstances was $maxInstances; kept ${keptLines.size} lines for training")
+        fileUtil.writeLinesToFile(newTrainingFile, keptLines)
       }
 
-      val testing_file = s"${fromSplitDir}${fixed}/testing.tsv"
-      if (fileUtil.fileExists(testing_file)) {
-        val testing_instances = DatasetReader.readNodePairFile(testing_file, Some(graph), fileUtil)
-        val filtered_testing_instances = testing_instances.instances
+      val testingFile = s"${fromSplitDir}${fixed}/testing.tsv"
+      if (fileUtil.fileExists(testingFile)) {
+        val testingInstances = DatasetReader.readNodePairFile(testingFile, Some(graph), fileUtil)
+        val filteredTestingInstances = testingInstances.instances
           .filterNot(i => i.source == -1 || i.target == -1)
-        val testing_data = new Dataset[NodePairInstance](filtered_testing_instances)
-        val negative_testing_instances = if (generateNegativesFor.contains("testing")) {
-          addNegativeExamples(testing_data, training_data.instances, relation, domains.toMap, ranges.toMap, graph.nodeDict)
+        val testingData = new Dataset[NodePairInstance](filteredTestingInstances)
+        val negativeTestingInstances = if (generateNegativesFor.contains("testing")) {
+          addNegativeExamples(testingData, trainingData.instances, relation, domains.toMap, ranges.toMap, graph.nodeDict)
         } else {
           new Dataset[NodePairInstance](Seq())
         }
-        val new_testing_file = s"${rel_dir}testing.tsv"
-        fileUtil.copy(testing_file, new_testing_file)
-        fileUtil.writeLinesToFile(
-          new_testing_file,
-          negative_testing_instances.instancesToStrings,
-          append=true
-        )
+        val newTestingFile = s"${relDir}testing.tsv"
+        val allLines = fileUtil.readLinesFromFile(testingFile) ++ negativeTestingInstances.instancesToStrings
+        val keptLines = maxInstances match {
+          case None => allLines
+          case Some(max) => random.shuffle(allLines).take(max)
+        }
+        logger.info(s"maxInstances was $maxInstances; kept ${keptLines.size} lines for testing")
+        fileUtil.writeLinesToFile(newTestingFile, keptLines)
       }
     }
     fileUtil.deleteFile(inProgressFile)
